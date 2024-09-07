419bc0a056234b1700879e8f3c5b4c3452116909
Nikita Nosov <90299797+nik1t7n@users.noreply.github.com>
2024-09-07 09:28:37 +0600

    Revert "Dev"

--- frontend/nebula/src/types/noteTypes.tsx
+++
@@ -1,9 +0,0 @@
-export type Mood = "happy" | "sad" | "neutral" | "excited" | "angry";
-
-export type Note = {
-  id: string;
-  content: string;
-  user_id: string;
-  mood: Mood;
-  created_at: string;
-}
